443bbb62573eb9ca352ff1a1eab7d231026ea49a
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-06-07 10:03:51 -0500

    chore: error

--- src/main/java/com/mycompany/ecommerce/antifraud/AntiFraudApplication.java
+++ src/main/java/com/mycompany/ecommerce/antifraud/AntiFraudApplication.java
@@ -57,7 +57,7 @@ public class AntiFraudApplication {
 	@RequestMapping(value = "/healthcheck", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Map healthcheck() {
 		// Comment out to generate a synthetic error
-		//generateError();
+		generateError();
 		return Collections.singletonMap("status", "Up");
 	}
 
@@ -68,9 +68,9 @@ public class AntiFraudApplication {
 
 	private void generateError() {
 		// Comment out to generate a synthetic error
-		/*if (pomVersion.equals("0.0.3")) {
+		if (pomVersion.equals("0.0.3")) {
 			// Generate a synthetic error if version is not right
 			throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE);
-		}*/
+		}
 	}
 }
